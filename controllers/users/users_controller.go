dd36c5caf4f00f8ed65194573dfc9084294de37e
mohit_jindal <mohit.jindal@keito.works>
2020-06-07 17:05:44 +0530

    Get functionlaity also added

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -9,6 +9,7 @@ import (
 	//"encoding/json"
 	"github.com/mohitjindal78/bookstore_users-api/services"
 	"github.com/mohitjindal78/bookstore_users-api/utils/errors"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context) {
@@ -45,5 +46,17 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "\nImplement Me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("use id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
